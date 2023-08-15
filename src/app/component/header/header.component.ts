d883b1a58cffcaeb505c101abd0b8c05c6948a04
apisit:name <apisit@multita.co.th>
2023-08-15 23:44:57 +0700

    Update : link icon

--- src/app/component/header/header.component.ts
+++ src/app/component/header/header.component.ts
@@ -7,9 +7,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
+  pdfUrl = "../../../assets/Apisit Purisan.pdf";
   ngOnInit() {
   }
-
+  
+  downloadPdf(): void {
+    const pdfUrl = '../../../assets/Apisit Purisan.pdf'; // Replace with the actual PDF URL
+    const link = document.createElement('a');
+    link.href = pdfUrl;
+    link.download = 'Apisit_Purisan.pdf'; // Specify the desired downloaded file name
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+  }
 
 
 
